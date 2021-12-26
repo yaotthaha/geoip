8b8bc6b9ab604d2ec78eec88a925cc40f9abeced
yaotthaha-vscode <95022881+yaotthaha@users.noreply.github.com>
2021-12-26 20:32:10 +0800

    Add Zerotier(IPv4/IPv6)

--- custom_rule.sh
+++ custom_rule.sh
@@ -11,6 +11,7 @@ curl -sL https://gaoyifan.github.io/china-operator-ip/cernet.txt >> data/cn-cern
 curl -sL https://gaoyifan.github.io/china-operator-ip/cernet6.txt >> data/cn-cernet
 if [ ! -f "data/cn-cernet" ]; then echo "[cn-cernet] IP List Add Fail!!"; exit 2; fi
 ### Add Zerotier Root Server IP
-curl -s "https://dns.google/resolve?name=root.zerotier.com&type=A" | jq -r .Answer | jq -r .[] | grep data | sed 's/^[ \t]*//g' | cut -d '"' -f4 | while read line; do echo $line >> zerotier.tmp; done
-curl -s "https://dns.google/resolve?name=root.zerotier.com&type=AAAA" | jq -r .Answer | jq -r .[] | grep data | sed 's/^[ \t]*//g' | cut -d '"' -f4 | while read line; do echo $line >> zerotier.tmp; done
-if [ "$(cat zerotier.tmp | wc -l)" != "0" ]; then mv zerotier.tmp data/zerotier; else echo "[zerotier] IP List Add Fail"; exit 2; fi
+curl -s "https://dns.google/resolve?name=root.zerotier.com&type=A" | jq -r .Answer | jq -r .[] | grep data | sed 's/^[ \t]*//g' | cut -d '"' -f4 | while read line; do echo $line >> zerotier4.tmp; done
+curl -s "https://dns.google/resolve?name=root.zerotier.com&type=AAAA" | jq -r .Answer | jq -r .[] | grep data | sed 's/^[ \t]*//g' | cut -d '"' -f4 | while read line; do echo $line >> zerotier6.tmp; done
+if [ "$(cat zerotier4.tmp | wc -l)" != "0" ]; then cat zerotier4.tmp >> data/zerotier; mv zerotier4.tmp data/zerotier4; else echo "[zerotier] IPv4 List Add Fail"; exit 2; fi
+if [ "$(cat zerotier6.tmp | wc -l)" != "0" ]; then cat zerotier6.tmp >> data/zerotier; mv zerotier6.tmp data/zerotier6; else echo "[zerotier] IPv6 List Add Fail"; exit 2; fi
